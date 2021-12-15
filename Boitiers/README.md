032afa6feb54096dcf0d7b0fc01eb509ec857737
BedinoTom <tom.bedino@etu.univ-cotedazur.fr>
2021-12-15 17:45:49 +0100

    Seance 2 v1
    -Ajout du rapport de seance du 15/12
    -Ajout des programmes du boitiers
    -Ajout instructions de debug sur l'IA

---
+++ Boitiers/README.md
@@ -0,0 +1,10 @@
+# Boitier
+Cette partie du git est réservé aux documents de développement du boitier.
+
+## Programme
+Voici la liste des progammes conçu pour le dirigeable:
+- [Capteur Doppler](doppler_test) : Programme permettant de détecter un mouvement par effet Doppler.
+- [Capteur Photodiode](lumen_test) : Programme permettant de lire le signal analogique 
+représentant la quantité de lumière.
+- [Timer](timer_test) : Programme permettant de sauvegarder et de lire la date.
+- [Camera Thermique](temperatureMatrix) : Programme permettant de récuperer la matrice thermique.
---
+++ Boitiers/doppler_test/doppler_test.ino
@@ -0,0 +1,38 @@
+int count=0;
+int timer=0;
+
+void setup() {
+
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(2), stateChange, FALLING);
+  timer=millis(); 
+}
+
+void loop() {
+  if((millis()-timer)>1000)
+  {
+    process();
+    timer=millis();
+  }
+}
+
+void stateChange()  //Interrupt function
+{  
+  count++;  
+
+}
+
+void process()   //Timer handler
+{
+  if(count>1)  
+//1000ms interrupt number greater than 1 is considered detected a moving object //(this value can be adjusted according to the actual situation, equivalent to //adjust the detection threshold of the speed of a moving object)
+       {
+                   Serial.println("Detect");
+                   count=0;   //Count zero
+       }
+        else
+        {
+            count=0; 
+        }
+//In 1000ms, interrupts does not reach set threshold value is considered not detect //moving objects, interrupt the count number is cleared to zero.  
+} 
